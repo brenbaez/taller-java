016ea9258e246952ac79a7d3c4c0b4609758bbaa
brenbaez <bbaez@grupoassa.com>
2020-04-19 16:17:46 -0300

    eje 1 generics

---
+++ generics/eje1/src/net/edu/isistan/taller/Animal.java
@@ -0,0 +1,38 @@
+package net.edu.isistan.taller;
+
+public class Animal {
+    private String title;
+    private String name;
+    private Integer weight;
+    private Integer age;
+
+    public Animal(String name, Integer weight, Integer age) {
+        this.name = name;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
